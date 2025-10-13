c0c5c79b17b1ed8d0543562558462e6bd240fe62
acost2678 <jenacosta2678@gmail.com>
2025-10-13 08:45:00 -0400

    Update login.py

--- login.py
+++ login.py
@@ -13,7 +13,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["password"]:
+    if st.session_state.get("password") == st.secrets["password"]:
             st.session_state["password_correct"] = True
             del st.session_state["password"]
         else:
@@ -33,4 +33,5 @@ def check_password():
 if check_password():
 
     st.switch_page("pages/app.py")
+
 
